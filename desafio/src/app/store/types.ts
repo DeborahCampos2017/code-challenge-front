834d5bc657bc6a766f776da00f47e0c838da1113
Deborah Campos <campos.deborahsousa@gmail.com>
2025-06-15 03:12:22 -0300

    feat: add useUserStore

---
+++ desafio/src/app/store/types.ts
@@ -0,0 +1,15 @@
+export interface PersonalData {
+  nome: string;
+  email: string;
+  telefone: string;
+  genero: string;
+}
+
+export interface AddressData {
+  endereco: string;
+  numero: string;
+  bairro: string;
+  cidade: string;
+  estado: string;
+  cep: string;
+}
